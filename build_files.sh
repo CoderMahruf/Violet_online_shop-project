f9249252f13d1816fe78a5f92a88bebb1b15ef0a
mahruf60 <mahruf9060@gmail.com>
2025-02-02 23:08:14 +0600

    commit

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,2 @@
-# echo "BUILD START"
-# python3.12 -m pip install -r requirements.txt
-# python3.12 manage.py collectstatic --noinput --clear
-# echo "BUILD END"
-
-#!/bin/bash
 pip install -r requirements.txt
-python3 manage.py collectstatic --noinput
+python3.9 manage.py collectstatic
--- config/settings.py
+++ config/settings.py
@@ -23,15 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-13appnn#4*(32)03_!rgr=#3q#bi^rs(vx=e-g0*r)7o7*jxzi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1','.now.sh']
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -46,7 +45,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,7 +123,7 @@ USE_TZ = True
 
 MEDIA_URL = 'media/'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 STATIC_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_ROOT = BASE_DIR / 'media'
 
